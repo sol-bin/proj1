628593f6bd12c17d98ef1eb30b4b56c472025d9c
sol-bin <76501209+sol-bin@users.noreply.github.com>
2020-12-23 15:57:44 +0900

    1223.1557

--- js/slide.js
+++ js/slide.js
@@ -12,4 +12,17 @@ $(".lazy").slick({
     slidesToScroll: 1//클릭할때 몇칸씩 넘어갈지
   });
 
+
+  $(function(){
+    var slide;
+    slide = setInterval(function(){
+        $('.notice .notice_list > li:first-child').animate({
+            'margin-top':'-2rem'},600,function(){
+          $('.notice .notice_list').append(
+          $('.notice .notice_list > li:first-child')); 
+          $('.notice .notice_list > li:last-child').css({'margin-top':0});
+            });
+    },2000);
+  })
+
 })//jq end
